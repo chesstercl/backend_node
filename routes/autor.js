a11bb0a60c728d39280ce92fcae1e3154081445d
Juan Barra <contacto@juanbarra.cl>
2021-10-16 17:55:21 -0300

    feat: geat autor route

--- routes/autor.js
+++ routes/autor.js
@@ -2,8 +2,13 @@ const {Router} = require("express");
 const express = require("express");
 const route = express.Router();
 
-const { postAutor } = require('../controllers/autor');
+const { postAutor, getAutor, getAutores } = require('../controllers/autor');
 
-route.route("/").post(postAutor);
+route.route("/")
+        .post(postAutor)
+        .get(getAutores)
+
+route.route("/:id")
+        .get(getAutor)
 
 module.exports =  route;
