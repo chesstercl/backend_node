67c9fa3ae9e3bc254186d6ca5440e7e5374ed604
Juan Barra <contacto@juanbarra.cl>
2021-10-16 16:42:41 -0300

    fix: postAutor controller

--- controllers/autor.js
+++ controllers/autor.js
@@ -1,6 +1,6 @@
 const Autor = require('../models/Autor')
 
-exports.crearAutor = async (req, res, next) => {
+exports.postAutor = async (req, res, next) => {
 
     const autorData = await Autor.create(req.body);
 
