d5984673c53eaeeb81e7ed1ab1a3814314ac2f3f
Juan Barra <contacto@juanbarra.cl>
2021-10-17 15:07:33 -0300

    refactor: libro controllers

--- controllers/libro.js
+++ controllers/libro.js
@@ -1,20 +1,84 @@
-exports.getLibros = (req, res, next) => {
-    res.status(200).json({ status:200, mensaje: 'Se proceso correctamente' });
+const Libro = require('../models/Libro');
+const ErrorResponse = require("../helper/errorResponse");
+
+exports.getLibros = async (req, res, next) => {
+    try{
+        const libros = await Libro.find();
+
+        res.status(200).json(libros)
+
+    }catch(err){
+        next(new ErrorResponse('No se pudo procesar el request' + err.message, 400));
+    }
 }
 
-exports.getLibro = (req, res, next) => {
-    res.status(200).json({ status:200, mensaje: 'Se devuelve el libro por id' });
+exports.getLibro = async (req, res, next) => {
+    try{
+        const libro = await Libro.findById(req.params.id);
+
+        if(!libro){
+            return next(
+                new ErrorResponse('No se pudo procesar el request', 400)
+                );
+        }
+        res.status(200).json(libros)
+
+    }catch(err){
+        next(new ErrorResponse('No se pudo procesar el request' + err.message, 400));
+    }
+    
 }
 
-exports.postLibro = (req, res, next) => {
-    res.status(200).json({ status:200, mensaje: 'Se ha creado el libro correctamente' });
+exports.postLibro = async (req, res, next) => {
+    try{
+        const libro = await Libro.create(req.body);
+
+        res.status(200).json({
+            status: 200,
+            data: libro
+        });
+
+        if(!libro){
+            return next(
+                new ErrorResponse('No se pudo procesar el request' + err.message, 400)
+            );
+        }
+    }catch(err){
+        next(new ErrorResponse('No se pudo procesar el request' + err.message, 400));
+    }
 }
 
-exports.putLibro = (req, res, next) => {
-    res.status(200).json({ status:200, mensaje: 'Se actualizo el libro' });
+exports.putLibro = async (req, res, next) => {
+    try{
+        const libro = await Libro.findByIdAndUpdate(req.params.id, req.body);
+
+        res.status(200).json({
+            status: 200,
+            data: libro
+        });
+
+    }catch(err){
+        next(new ErrorResponse('No se pudo procesar el request' + err.message, 400));
+    }
 }
 
-exports.deleteLibro = (req, res, next) => {
-    res.status(200).json({ status:200, mensaje: 'Se elimino el libro' });
+exports.deleteLibro = async (req, res, next) => {
+    try{
+        const libro = await Libro.findByIdAndDelete(req.params.id);
+
+        if(!libro){
+            return next(
+                new ErrorResponse('EL libro no existe', 400)
+            );
+        }
+
+        res.status(200).json({
+            status: 200,
+            data: libro
+        });
+
+    }catch(err){
+        next(new ErrorResponse('No se pudo procesar el request' + err.message, 400));
+    }
 }
 
