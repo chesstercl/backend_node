222184b992e497e0ca859f5fb25eccd4145691dc
Juan Barra <contacto@juanbarra.cl>
2021-10-17 21:45:53 -0300

    feat: add usuario res

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,11 +1,28 @@
-exports.registrarUsuario = (req, res, next) => {
+const Usuario = require("../models/Usuario");
+const ErrorResponse = require("../helper/errorResponse");
+
+exports.registrarUsuario = async (req, res, next) => {
+  try {
+    const { nombre, apellido, username, email, password } = req.body;
+    await Usuario.create({
+      nombre,
+      apellido,
+      userName: username,
+      email,
+      password,
+    });
     res.status(200).json({status:200});
-}
+  } catch (err) {
+      next(
+          new ErrorResponse("Error registrando usuario" + err, 400)
+      )
+  }
+};
 
 exports.login = (req, res, next) => {
-    res.status(200).json({status:200});
-}
+  res.status(200).json({ status: 200 });
+};
 
 exports.getUsuario = (req, res, next) => {
-    res.status(200).json({status:200});
-}
+  res.status(200).json({ status: 200 });
+};
