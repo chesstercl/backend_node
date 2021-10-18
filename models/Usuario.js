d7671fb309e120d42976b75a8420b4436424dd11
Juan Barra <contacto@juanbarra.cl>
2021-10-17 21:58:03 -0300

    feat: add bcrypt

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,33 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UsuarioSchema = new mongoose.Schema({
-    nombre: {
-        type: String,
-        required: [true, 'Por favor ingrese un nombre']
-    },
-    apellido: {
-        type: String,
-        required: [true, 'Por favor ingrese un apellido']
-    },
-    userName: {
-        type: String,
-        required: [true, 'Por favor ingrese un username']
-    },
-    email: {
-        type: String,
-        required: [true, 'Por favor ingrese un email'],
-        unique: true,
-        match: [
-            /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-            'Ingrese un email valido'
-        ]
-    },
-    password: {
-        type: String,
-        required: [true, 'Por favor ingrese un password'],
-        minlength: 6,
-        select: false
-    }
+  nombre: {
+    type: String,
+    required: [true, "Por favor ingrese un nombre"],
+  },
+  apellido: {
+    type: String,
+    required: [true, "Por favor ingrese un apellido"],
+  },
+  userName: {
+    type: String,
+    required: [true, "Por favor ingrese un username"],
+  },
+  email: {
+    type: String,
+    required: [true, "Por favor ingrese un email"],
+    unique: true,
+    match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Ingrese un email valido"],
+  },
+  password: {
+    type: String,
+    required: [true, "Por favor ingrese un password"],
+    minlength: 6,
+    select: false,
+  },
 });
 
-module.exports = mongoose.model('Usuario', UsuarioSchema);
+UsuarioSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("Usuario", UsuarioSchema);
